a5b6ebc64f9ef5e764ba90719302bc3d04b5d412
juls-ca <biay@ualberta.ca>
2023-06-28 21:08:14 -0700

    Clean up nesting + add translation text domain

--- page-faq.php
+++ page-faq.php
@@ -16,53 +16,53 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+
 		<header class="page-header">
-			<h1 class="page-title"><?php the_title() ?></h1>	
+			<!-- Page title and overview (acf) -->
+			<h1 class="page-title"><?php the_title() ?></h1> <?php
+
+			if( function_exists('get_field') ) :
+				if( get_field('faq_page_overview') ) : ?>
+					<p><?php the_field('faq_page_overview') ?></p> <?php
+				endif;
+			endif; ?>
 		</header>
 
 		<?php 
-			if( function_exists('get_field') ) :
-				if( get_field('faq_page_overview') ) : ?>
-					<p><?php the_field('faq_page_overview') ?></p>
-				<?php
-				endif;
+		// Get FAQ CPT
+		$args = array (
+			'post_type'     => 'whi-faq',
+			'orderby'			 	=> 'date',
+			'order'			 		=> 'asc',
+		);
+		$query = new WP_Query($args); 
 
-				// Get FAQ CPT
-				$args = array (
-					'post_type'     => 'whi-faq',
-					'orderby'			 	=> 'date',
-					'order'			 		=> 'asc',
-				);
-				$query = new WP_Query($args); 
+		if( $query -> have_posts() ) :
+			while ( $query->have_posts() ) :
+				$query -> the_post(); ?>
+				<section>
+					<h2><?php the_title(); ?></h2> <?php
+					
+					// Check if ACF Repeater rows exist and loop 
+					if( function_exists('get_field') && have_rows( 'faq_repeater' ) ) :
+						while( have_rows( 'faq_repeater' ) ) : the_row() ?>
+							<div class="single-qa">
+								<h3><?php the_sub_field( 'faq_question' ); ?></h3>
+								<p><?php the_sub_field( 'faq_answer' ); ?></p>
+							</div> <?php
+						endwhile;
+					
+					else: ?>
+					<p><?php _e( 'Sorry, no FAQs to display.', 'whistler-cabins' ); ?></p> <?php
+					endif; ?> 
 
-				if( $query -> have_posts() ) :
-					while ( $query->have_posts() ) :
-						$query -> the_post(); ?>
-						<section>
-							<h2><?php the_title(); ?></h2>
-							<?php
-							// Check if ACF Repeater rows exist and loop 
-							if( have_rows( 'faq_repeater' ) ) :
-								while( have_rows( 'faq_repeater' ) ) : the_row() ?>
-									<div class="single-qa">
-										<h3><?php the_sub_field( 'faq_question' ); ?></h3>
-										<p><?php the_sub_field( 'faq_answer' ); ?></p>
-									</div>
-								<?php
-								endwhile;
-							endif;
-							?> 
-						</section>
-						<?php
-					endwhile;
-					wp_reset_postdata();
+				</section> <?php
+			endwhile;
+			wp_reset_postdata();
 
-				else: ?>
-					<p><?php _e( 'Sorry, no FAQs available.' ); ?></p>
-				<?php
-				endif;
-			endif;
-		?>
+		else: ?>
+			<p><?php _e( 'Sorry, no FAQs to display.', 'whistler-cabins' ); ?></p> <?php
+		endif; ?>
 
 	</main>
 
