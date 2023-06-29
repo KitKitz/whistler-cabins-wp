7bf5f7a97dfc84d2e639a7a65c161808189b178e
juls-ca <biay@ualberta.ca>
2023-06-28 20:40:06 -0700

    Retrieve FAQ posts

--- page-faq.php
+++ page-faq.php
@@ -16,22 +16,43 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+		<header class="page-header">
+			<h1 class="page-title"><?php the_title() ?></h1>	
+		</header>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		<?php 
+			if( function_exists('get_field') ) :
+				if( get_field('faq_page_overview') ) : ?>
+					<p><?php the_field('faq_page_overview') ?></p>
+				<?php
+				endif;
 
-			get_template_part( 'template-parts/content', 'page' );
+				// Get FAQ CPT
+				$args = array (
+					'post_type'     => 'whi-faq',
+					'orderby'			 	=> 'date',
+					'order'			 		=> 'asc',
+				);
+				$query = new WP_Query($args); 
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
+				if( $query -> have_posts() ) :
+					while ( $query->have_posts() ) :
+						$query -> the_post(); ?>
+						<section>
+							<h2><?php the_title(); ?></h2> 
+						</section>
+						<?php
+					endwhile;
+					wp_reset_postdata();
+
+				else: ?>
+					<p><?php _e( 'Sorry, no FAQs available.' ); ?></p>
+				<?php
+				endif;
 			endif;
-
-		endwhile; // End of the loop.
 		?>
 
-	</main><!-- #main -->
+	</main>
 
 <?php
 get_sidebar();
