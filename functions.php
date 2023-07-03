d0cdc1e354fa5b5694131e2efea1b18c66b6b9cc
Michelle Chan <c_michellec@hotmail.com>
2023-07-03 00:07:14 -0700

    displaying categories on shop page/ button text

--- functions.php
+++ functions.php
@@ -267,5 +267,35 @@ function whistler_cabins_shop_init(){
 		31
 	);
 
+	function whistler_cabins_category($category_ids) {
+		if (is_shop()) {
+			global $product;
+			$product_categories = wp_get_post_terms($product->get_id(), 'product_cat', array('fields' => 'ids'));
+	
+			// Check if any of the product's categories match the specified category IDs
+			$matches = array_intersect($product_categories, $category_ids);
+	
+			if (!empty($matches)) {
+				// Loop through the matched categories and display them
+				foreach ($matches as $cat_id) {
+					$category = get_term($cat_id, 'product_cat');
+					?><p><a href="<?php echo get_term_link($category)?>"><?php echo $category->name; ?></a></p><?php
+				}
+			}
+		}
+	}
+	
+	add_action('woocommerce_after_shop_loop_item_title', 
+				function() {
+					whistler_cabins_category(array(25, 27, 44, 45, 46, 47));
+				}, 
+				11
+	);
+
+	add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_add_to_cart_button_text_archives' );  
+	function woocommerce_add_to_cart_button_text_archives() {
+    return __( 'View Cabin', 'woocommerce' );
+	}
+
 }
 add_action('init', 'whistler_cabins_shop_init');
