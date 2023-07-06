d6af7469b12af5635b6864ed90d7b669eb779b53
Caitlyn Wen <canwen1998@gmail.com>
2023-07-06 12:34:08 -0700

    Wrap business info for styling

--- page-contact.php
+++ page-contact.php
@@ -23,35 +23,48 @@ get_header();
 
 			get_template_part( 'template-parts/content', 'page' );
 
-			if (function_exists('get_field')) {
-				if (get_field('business_address')) {
-					$address = get_field('business_address');
-					echo '<p>Address: ' . $address . '</p>';
-				}
-				
-				if (get_field('business_phone')) {
-					$phone = get_field('business_phone');
-					echo '<p>Phone: ' . $phone . '</p>';
-				}
-				
-				if (get_field('business_email')) {
-					$email = get_field('business_email');
-					echo '<p>Email: <a href="mailto:' . $email . '">' . $email . '</a></p>';
-				}
-				
-				$contact_map = get_field('contact_map');
-				$map_icon = get_field('contact_map_icon');
-				if( $contact_map ): ?>
-					<div class="acf-map" data-zoom="16">
-						<div class="marker" 
-						data-lat="<?php echo esc_attr($contact_map['lat']); ?>" 
-						data-lng="<?php echo esc_attr($contact_map['lng']); ?>"
-						data-icon="<?php echo $map_icon?>"></div>
-					</div>
-				<?php endif; 
-			}
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
 
 		endwhile; // End of the loop.
+	?>
+
+	<?php
+	if (function_exists('get_field')) {
+		?>
+		<div class="contact-page-business-info">
+		<?php
+			if (get_field('business_address')) {
+				$address = get_field('business_address');
+				echo '<p>Address: ' . $address . '</p>';
+			}
+			
+			if (get_field('business_phone')) {
+				$phone = get_field('business_phone');
+				echo '<p>Phone: ' . $phone . '</p>';
+			}
+			
+			if (get_field('business_email')) {
+				$email = get_field('business_email');
+				echo '<p>Email: <a href="mailto:' . $email . '">' . $email . '</a></p>';
+			}
+		?>
+		</div>
+
+		<?php
+		$contact_map = get_field('contact_map');
+		$map_icon = get_field('contact_map_icon');
+		if( $contact_map ): ?>
+			<div class="acf-map contact-page-map" data-zoom="16">
+				<div class="marker" 
+				data-lat="<?php echo esc_attr($contact_map['lat']); ?>" 
+				data-lng="<?php echo esc_attr($contact_map['lng']); ?>"
+				data-icon="<?php echo $map_icon?>"></div>
+			</div>
+		<?php endif; 
+	}
 	?>
 
 	</main><!-- #main -->
