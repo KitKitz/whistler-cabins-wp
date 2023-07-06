622777d686b289d70ad3f83f78ec8d68f2e8b575
juls-ca <biay@ualberta.ca>
2023-07-06 13:35:56 -0700

    Remove Custom Header support

--- functions.php
+++ functions.php
@@ -180,11 +180,6 @@ function whistler_cabins_scripts() {
 add_action( 'wp_enqueue_scripts', 'whistler_cabins_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
