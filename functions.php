89820a1f1aea7588cfc49fab8d0ce46d7dd766b9
juls-ca <biay@ualberta.ca>
2023-07-10 18:39:06 -0700

    Add footer social menu + content

--- functions.php
+++ functions.php
@@ -51,6 +51,7 @@ function whistler_cabins_setup() {
 		array(
 			'menu-1' => esc_html__( 'Primary', 'whistler-cabins' ),
 			'menu-2' => esc_html__( 'Footer', 'whistler-cabins' ),
+			'menu-3' => esc_html__( 'Footer Social', 'whistler-cabins' ),
 		)
 	);
 
