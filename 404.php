7ff9c28880adb7486ea2a1c91705b2437fc7cdf2
Caitlyn Wen <canwen1998@gmail.com>
2023-07-17 02:02:42 -0700

    Page 404 and style

--- 404.php
+++ 404.php
@@ -12,47 +12,63 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
+		<div class="error-404 not-found">
 			<header class="page-header">
 				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'whistler-cabins' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'whistler-cabins' ); ?></p>
-
+			<p>
+				<?php
+				esc_html_e( 'Sorry, we can&rsquo;t find the page you&rsquo;re looking for. Please use the links below to access popular pages, or heading to our', 'whistler-cabins' );
+				echo ' <a href="' . esc_url( home_url( '/' ) ) . '">' . esc_html__( 'home page', 'whistler-cabins' ) . '</a>.';
+				?>
+			</p>
+				<section class="products-categories">
+					<h2 class="products-categories-title"><?php esc_html_e( 'Cabin Categories', 'whistler-cabins' ); ?></h2>
 					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'whistler-cabins' ); ?></h2>
+					$cabins_categories = get_terms(
+						array(
+							'taxonomy' => 'product_cat',
+							'orderby' => 'count',
+							'order' => 'DESC',
+							'number' => 10,
+							'hide_empty' => true,
+						)
+					);
+	
+					if (!empty($cabins_categories) && !is_wp_error($cabins_categories)) : ?>
 						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
+							<?php foreach ($cabins_categories as $category) : ?>
+								<?php if ($category->term_id === 18) continue; ?>
+								<li>
+									<a href="<?php echo get_term_link($category); ?>"><?php echo $category->name; ?></a>
+								</li>
+							<?php endforeach; ?>
 						</ul>
-					</div><!-- .widget -->
+					<?php endif; ?>
+				</section>
+				<!-- .product categories -->
 
-					<?php
-					/* translators: %1$s: smiley */
-					$whistler_cabins_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'whistler-cabins' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$whistler_cabins_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+				<section class="widget widget_categories">
+					<h2 class="widget-title"><?php esc_html_e( 'Popular Activities', 'whistler-cabins' ); ?></h2>
+					<ul>
+						<?php
+						wp_list_categories(
+							array(
+								'orderby'    => 'count',
+								'order'      => 'DESC',
+								'show_count' => 1,
+								'title_li'   => '',
+								'number'     => 10,
+							)
+						);
+						?>
+					</ul>
+				</section>
+				<!-- .widget -->
 			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+		</div><!-- .error-404 -->
 
 	</main><!-- #main -->
 
